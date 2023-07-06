af7ac45ffbc2ecd10a28e6512302ee04ef23e1db
Andrei ~Tache <takeandrei5@gmail.com>
2023-07-06 22:26:10 +0300

    feat: adjustment to text editor

--- webui/src/vite-env.d.ts
+++ webui/src/vite-env.d.ts
@@ -6,7 +6,11 @@ interface ImportMetaEnv {
 =======
   readonly VITE_AUTH0_DOMAIN: string
   readonly VITE_AUTH0_CLIENT_ID: string
+<<<<<<< HEAD
 >>>>>>> cc84012 (feature/at/add-sign-up-with-auth0)
+=======
+  readonly VITE_TINY_MCE_API_KEY: string
+>>>>>>> c25480a (feat: adjustment to text editor)
 }
 
 interface ImportMeta {
