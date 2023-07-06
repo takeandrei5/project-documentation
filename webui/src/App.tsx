cc84012873d8960d047f37541c4678b8aaa3930b
Andrei Tache <takeandrei5@gmail.com>
2023-07-06 23:02:48 +0300

    feature/at/add-sign-up-with-auth0

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,17 +1,26 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import { RouterProvider } from '@tanstack/router';
 import { router } from './router';
 
 function App() {
+
 	return (
-    <RouterProvider router={router} />
+		<Auth0Provider
+			domain={import.meta.env.VITE_AUTH0_DOMAIN}
+			clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+			authorizationParams={{
+				redirect_uri: window.location.origin
+			}}>
+			<RouterProvider router={router} />
+		</Auth0Provider>
 	);
 }
 
 declare module '@tanstack/router' {
-  interface Register {
-    // This infers the type of our router and registers it across your entire project
-    router: typeof router
-  }
+	interface Register {
+		// This infers the type of our router and registers it across your entire project
+		router: typeof router;
+	}
 }
 
 export default App;
