6bf18a19fa240f02eb5ecceb585e92a8a40a9523
Andrei Tache <takeandrei5@gmail.com>
2023-07-06 23:14:31 +0300

    feat: adjustment to text editor

--- webui/src/vite-env.d.ts
+++ webui/src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_TINY_MCE_API_KEY: string
   readonly VITE_AUTH0_DOMAIN: string
   readonly VITE_AUTH0_CLIENT_ID: string
 }
