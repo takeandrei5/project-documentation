09f486c659b261ed63dbe5e9f42dfdae2f362bb3
Andrei Tache <takeandrei5@gmail.com>
2023-07-06 23:02:48 +0300

    feature/at/add-sign-up-with-auth0

--- webui/src/pages/HomePage.tsx
+++ webui/src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 const HomePage: React.FC = () => {
-  return <div>Hello from home!</div>
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return <>
+    <div>Hello from home!</div>
+      {!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>}
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+    </>
+
 }
 
 export default HomePage;
--- webui/src/vite-env.d.ts
+++ webui/src/vite-env.d.ts
@@ -1,7 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+<<<<<<< HEAD
   readonly VITE_TINY_MCE_API_KEY: string
+=======
+  readonly VITE_AUTH0_DOMAIN: string
+  readonly VITE_AUTH0_CLIENT_ID: string
+>>>>>>> cc84012 (feature/at/add-sign-up-with-auth0)
 }
 
 interface ImportMeta {
