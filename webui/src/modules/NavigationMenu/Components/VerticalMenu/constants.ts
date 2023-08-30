ccfc29154da1b01b7fa208b240d5792a3cee3e73
Andrei Tache <takeandrei5@gmail.com>
2023-08-30 21:20:37 +0300

    feat: added rest of the snackbar messages (#12)

---
+++ webui/src/modules/NavigationMenu/Components/VerticalMenu/constants.ts
@@ -0,0 +1,10 @@
+import { type SnackbarMessages } from './types';
+
+const snackbarMessages: SnackbarMessages = {
+  copy: 'Link copied to clipboard',
+  delete: 'Item deleted',
+  duplicate: 'Item duplicated',
+  rename: 'Item renamed',
+}
+
+export { snackbarMessages };
--- webui/src/modules/NavigationMenu/Components/VerticalMenu/types.ts
+++ webui/src/modules/NavigationMenu/Components/VerticalMenu/types.ts
@@ -1,9 +1,17 @@
+import { type NodeModel } from '@minoru/react-dnd-treeview';
 import { type Dispatch } from 'react';
-import { type NodeModel } from '../../types';
+import { type TreeDataValues } from '../../types';
 
 export type VerticalMenuProps = {
-	setTreeData: Dispatch<React.SetStateAction<NodeModel[]>>;
+	setTreeData: Dispatch<React.SetStateAction<NodeModel<TreeDataValues>[]>>;
 	nodeId: number;
 	text: string;
-	treeData: NodeModel[];
+	treeData: NodeModel<TreeDataValues>[];
 };
+
+export type SnackbarMessages = {
+  copy: string;
+  delete: string;
+  duplicate: string;
+  rename: string;
+}
