4de6e7b6a672f3e688b128acf5359b56dc813c31
Andrei ~Tache <takeandrei5@gmail.com>
2023-09-07 16:29:46 +0300

    style: added theme colors

--- webui/src/assets/theme.ts
+++ webui/src/assets/theme.ts
@@ -2,53 +2,104 @@ import { createTheme } from '@mui/material';
 
 export const mainTheme = createTheme({
 	palette: {
-		primary: {
-			light: '#7fd9f1',
-			main: '#0b717f',
-			dark: '#25b9cf',
-			contrastText: '#FFFFFF'
-		},
-		secondary: {
-			light: '#FF0000',
-			main: '#CC4949',
-			dark: '#sb10303'
-		},
-		action: {
-			//      disabledBackground: '#b4b4b4',
-			//      disabled: '#FFFFFF',
-		},
-		info: {
-			light: '#709fea',
-			main: '#3E64D7',
-			dark: '#2d41a0'
-		},
-		success: {
-			light: '#33f57c',
-			main: '#00ab41',
-			dark: '#0a7132'
-		},
-		error: {
-			light: '#ff5757',
-			main: '#FF0000',
-			dark: '#b10303'
-		},
-		warning: {
-			light: '#ffda46',
-			main: '#ffa500',
-			dark: '#bb5502'
-		},
+		purple: {
+      100: '#110D59',
+      80: '#28246F',
+      60: '#44408A',
+      40: '#7E7BB2',
+      20: '#B4B1E5',
+      10: '#DAD9F5'
+    },
+    blue: {
+      100: '#08437A',
+      80: '#0B5FAE',
+      60: '#0E77D9',
+      40: '#3F9CF2',
+      20: '#A3D1FC',
+      10: '#CFE8FF'
+    },
+    cyan: {
+      100: '#005F84',
+      80: '#2086AD',
+      60: '#3FA0C6',
+      40: '#6AB8D6',
+      20: '#9FD1E4',
+      10: '#D5ECF5'
+    },
+    greenLight: {
+      100: '#039267',
+      80: '#1AC391',
+      60: '#55E7BB',
+      40: '#95F4D8',
+      20: '#C7F8E9',
+      10: '#D9FCF1'
+    },
+    greenDark: {
+      100: '#49950D',
+      80: '#5FB918',
+      60: '#76DE24',
+      40: '#9AEC59',
+      20: '#C2F799',
+      10: '#DDFBC5'
+    },
+    yellow: {
+      100: '#D39405',
+      80: '#ECAA18',
+      60: '#FFBC26',
+      40: '#FFCC5A',
+      20: '#FDE2A1',
+      10: '#FCE9C0'
+    },
+    orange: {
+      100: '#DF6F0B',
+      80: '#F18524',
+      60: '#FEB371',
+      40: '#F9BC87',
+      20: '#FDD5B2',
+      10: '#FFECDC'
+    },
+    red: {
+      100: '#980808',
+      80: '#C91616',
+      60: '#F14D4D',
+      40: '#FF7878',
+      20: '#FBB4B4',
+      10: '#FCDBDB'
+    },
+    magenta: {
+      100: '#930C6D',
+      80: '#C11390',
+      60: '#E924B1',
+      40: '#F173CE',
+      20: '#F8B0E4',
+      10: '#FED5F2'
+    },
+    lightPurple: {
+      100: '#800C93',
+      80: '#AF20C7',
+      60: '#C750DB',
+      40: '#D97DE9',
+      20: '#E3A7ED',
+      10: '#F3CEFA'
+    },
+    textColor: {
+      100: '#2B283D',
+      80: '#4C4958',
+      60: '#55555F',
+      40: '#7D7A89',
+      20: '#B3B1BB',
+      0: '#FFFFFF'
+    },
 		background: {
-			default: '#F5F5F5',
-			//      paper: '#F9FBFD'
-			paper: '#FFFFFF'
+			default: '#F1F2F4'
 		},
 		common: {
 			white: '#FFFFFF',
-			black: '#000000'
+			black: '#2B283D'
 		}
 	},
 	typography: {
-		fontFamily: 'Roboto, sans-serif',
+		fontFamily: 'Inter, sans-serif',
 		subtitle1: {
 			fontSize: '1.25rem'
 		},
---
+++ webui/src/material-ui.d.ts
@@ -0,0 +1,94 @@
+import type { PaletteOptions } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  export interface PaletteOptions {
+    purple: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    blue: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    cyan: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    greenLight: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    greenDark: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    yellow: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    orange: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    red: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    magenta: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    lightPurple: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      10: string;
+    },
+    textColor: {
+      100: string;
+      80: string;
+      60: string;
+      40: string;
+      20: string;
+      0: string;
+    }
+  }
+}
