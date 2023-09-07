d0c734ee48afd6c273d36b303da76ada1227e7bf
Andrei ~Tache <takeandrei5@gmail.com>
2023-09-07 23:37:08 +0300

    style: added typography styles

--- webui/src/assets/theme.ts
+++ webui/src/assets/theme.ts
@@ -3,93 +3,93 @@ import { createTheme } from '@mui/material';
 export const mainTheme = createTheme({
 	palette: {
 		purple: {
-      100: '#110D59',
-      80: '#28246F',
-      60: '#44408A',
-      40: '#7E7BB2',
-      20: '#B4B1E5',
-      10: '#DAD9F5'
-    },
-    blue: {
-      100: '#08437A',
-      80: '#0B5FAE',
-      60: '#0E77D9',
-      40: '#3F9CF2',
-      20: '#A3D1FC',
-      10: '#CFE8FF'
-    },
-    cyan: {
-      100: '#005F84',
-      80: '#2086AD',
-      60: '#3FA0C6',
-      40: '#6AB8D6',
-      20: '#9FD1E4',
-      10: '#D5ECF5'
-    },
-    greenLight: {
-      100: '#039267',
-      80: '#1AC391',
-      60: '#55E7BB',
-      40: '#95F4D8',
-      20: '#C7F8E9',
-      10: '#D9FCF1'
-    },
-    greenDark: {
-      100: '#49950D',
-      80: '#5FB918',
-      60: '#76DE24',
-      40: '#9AEC59',
-      20: '#C2F799',
-      10: '#DDFBC5'
-    },
-    yellow: {
-      100: '#D39405',
-      80: '#ECAA18',
-      60: '#FFBC26',
-      40: '#FFCC5A',
-      20: '#FDE2A1',
-      10: '#FCE9C0'
-    },
-    orange: {
-      100: '#DF6F0B',
-      80: '#F18524',
-      60: '#FEB371',
-      40: '#F9BC87',
-      20: '#FDD5B2',
-      10: '#FFECDC'
-    },
-    red: {
-      100: '#980808',
-      80: '#C91616',
-      60: '#F14D4D',
-      40: '#FF7878',
-      20: '#FBB4B4',
-      10: '#FCDBDB'
-    },
-    magenta: {
-      100: '#930C6D',
-      80: '#C11390',
-      60: '#E924B1',
-      40: '#F173CE',
-      20: '#F8B0E4',
-      10: '#FED5F2'
-    },
-    lightPurple: {
-      100: '#800C93',
-      80: '#AF20C7',
-      60: '#C750DB',
-      40: '#D97DE9',
-      20: '#E3A7ED',
-      10: '#F3CEFA'
-    },
-    textColor: {
-      100: '#2B283D',
-      80: '#4C4958',
-      60: '#55555F',
-      40: '#7D7A89',
-      20: '#B3B1BB',
-      0: '#FFFFFF'
-    },
+			100: '#110D59',
+			80: '#28246F',
+			60: '#44408A',
+			40: '#7E7BB2',
+			20: '#B4B1E5',
+			10: '#DAD9F5'
+		},
+		blue: {
+			100: '#08437A',
+			80: '#0B5FAE',
+			60: '#0E77D9',
+			40: '#3F9CF2',
+			20: '#A3D1FC',
+			10: '#CFE8FF'
+		},
+		cyan: {
+			100: '#005F84',
+			80: '#2086AD',
+			60: '#3FA0C6',
+			40: '#6AB8D6',
+			20: '#9FD1E4',
+			10: '#D5ECF5'
+		},
+		greenLight: {
+			100: '#039267',
+			80: '#1AC391',
+			60: '#55E7BB',
+			40: '#95F4D8',
+			20: '#C7F8E9',
+			10: '#D9FCF1'
+		},
+		greenDark: {
+			100: '#49950D',
+			80: '#5FB918',
+			60: '#76DE24',
+			40: '#9AEC59',
+			20: '#C2F799',
+			10: '#DDFBC5'
+		},
+		yellow: {
+			100: '#D39405',
+			80: '#ECAA18',
+			60: '#FFBC26',
+			40: '#FFCC5A',
+			20: '#FDE2A1',
+			10: '#FCE9C0'
+		},
+		orange: {
+			100: '#DF6F0B',
+			80: '#F18524',
+			60: '#FEB371',
+			40: '#F9BC87',
+			20: '#FDD5B2',
+			10: '#FFECDC'
+		},
+		red: {
+			100: '#980808',
+			80: '#C91616',
+			60: '#F14D4D',
+			40: '#FF7878',
+			20: '#FBB4B4',
+			10: '#FCDBDB'
+		},
+		magenta: {
+			100: '#930C6D',
+			80: '#C11390',
+			60: '#E924B1',
+			40: '#F173CE',
+			20: '#F8B0E4',
+			10: '#FED5F2'
+		},
+		lightPurple: {
+			100: '#800C93',
+			80: '#AF20C7',
+			60: '#C750DB',
+			40: '#D97DE9',
+			20: '#E3A7ED',
+			10: '#F3CEFA'
+		},
+		textColor: {
+			100: '#2B283D',
+			80: '#4C4958',
+			60: '#55555F',
+			40: '#7D7A89',
+			20: '#B3B1BB',
+			0: '#FFFFFF'
+		},
 		background: {
 			default: '#F1F2F4'
 		},
@@ -97,27 +97,139 @@ export const mainTheme = createTheme({
 			white: '#FFFFFF',
 			black: '#2B283D'
 		},
-    disabled: {
-      default: '#E3E4E8'
-    }
+		disabled: {
+			default: '#E3E4E8'
+		}
 	},
 	typography: {
 		fontFamily: 'Manrope, sans-serif',
-		subtitle1: {
-			fontSize: '1.25rem'
+		extraSmall: {
+			regular: {
+				color: '#2B283D',
+				fontSize: '0.75rem',
+				fontStyle: 'normal',
+				fontWeight: 400,
+				lineHeight: '1.125rem',
+				letterSpacing: '0.015rem'
+			},
+			medium: {
+				color: '#2B283D',
+				fontSize: '0.75rem',
+				fontStyle: 'normal',
+				fontWeight: 500,
+				lineHeight: '1.125rem',
+				letterSpacing: '0.015rem'
+			},
+			semiBold: {
+				color: '#2B283D',
+				fontSize: '0.75rem',
+				fontStyle: 'normal',
+				fontWeight: 600,
+				lineHeight: '1.125rem',
+				letterSpacing: '0.015rem'
+			},
+			bold: {
+				color: '#2B283D',
+				fontSize: '0.75rem',
+				fontStyle: 'normal',
+				fontWeight: 700,
+				lineHeight: '1.125rem',
+				letterSpacing: '0.015rem'
+			}
 		},
-		subtitle2: {
-			fontSize: '1rem'
+		small: {
+			regular: {
+				color: '#2B283D',
+				fontSize: '0.875rem',
+				fontStyle: 'normal',
+				fontWeight: 400,
+				lineHeight: '1.3125rem',
+				letterSpacing: '0.0175rem'
+			},
+			medium: {
+				color: '#2B283D',
+				fontSize: '0.875rem',
+				fontStyle: 'normal',
+				fontWeight: 500,
+				lineHeight: '1.3125rem',
+				letterSpacing: '0.0175rem'
+			},
+			semiBold: {
+				color: '#2B283D',
+				fontSize: '0.875rem',
+				fontStyle: 'normal',
+				fontWeight: 600,
+				lineHeight: '1.3125rem',
+				letterSpacing: '0.0175rem'
+			},
+			bold: {
+				color: '#2B283D',
+				fontSize: '0.875rem',
+				fontStyle: 'normal',
+				fontWeight: 700,
+				lineHeight: '1.3125rem',
+				letterSpacing: '0.0175rem'
+			}
 		},
-		body1: {
-			fontSize: '0.875rem'
+		medium: {
+			regular: {
+				color: '#2B283D',
+				fontSize: '1rem',
+				fontStyle: 'normal',
+				fontWeight: 400,
+				lineHeight: '1.3rem',
+				letterSpacing: '0.02rem'
+			},
+			medium: {
+				color: '#2B283D',
+				fontSize: '1rem',
+				fontStyle: 'normal',
+				fontWeight: 500,
+				lineHeight: '1.3rem',
+				letterSpacing: '0.02rem'
+			},
+			semiBold: {
+				color: '#2B283D',
+				fontSize: '1rem',
+				fontStyle: 'normal',
+				fontWeight: 600,
+				lineHeight: '1.3rem',
+				letterSpacing: '0.02rem'
+			},
+			bold: {
+				color: '#2B283D',
+				fontSize: '1rem',
+				fontStyle: 'normal',
+				fontWeight: 700,
+				lineHeight: '1.3rem',
+				letterSpacing: '0.02rem'
+			}
 		},
-		body2: {
-			fontSize: '0.75rem'
+		large: {
+			regular: {
+				color: '#2B283D',
+				fontSize: '1.25rem',
+				fontStyle: 'normal',
+				fontWeight: 400,
+				lineHeight: '1.5rem',
+				letterSpacing: '0.025rem'
+			},
+			medium: { color: '#2B283D', fontSize: '1.25rem', fontStyle: 'normal', fontWeight: 500, lineHeight: '1.5rem', letterSpacing: '0.025rem' },
+			semiBold: { color: '#2B283D', fontSize: '1.25rem', fontStyle: 'normal', fontWeight: 600, lineHeight: '1.5rem', letterSpacing: '0.025rem' },
+			bold: { color: '#2B283D', fontSize: '1.25rem', fontStyle: 'normal', fontWeight: 700, lineHeight: '1.5rem', letterSpacing: '0.025rem' }
 		},
-		button: {
-			fontSize: '0.875rem',
-			fontWeight: 500
+		extraLarge: {
+			regular: {
+				color: '#2B283D',
+				fontSize: '1.625rem',
+				fontStyle: 'normal',
+				fontWeight: 400,
+				lineHeight: '1.95rem',
+				letterSpacing: '0.0325rem'
+			},
+			medium: { color: '#2B283D', fontSize: '1.625rem', fontStyle: 'normal', fontWeight: 500, lineHeight: '1.95rem', letterSpacing: '0.0325rem' },
+			semiBold: { color: '#2B283D', fontSize: '1.625rem', fontStyle: 'normal', fontWeight: 600, lineHeight: '1.95rem', letterSpacing: '0.0325rem' },
+			bold: { color: '#2B283D', fontSize: '1.625rem', fontStyle: 'normal', fontWeight: 700, lineHeight: '1.95rem', letterSpacing: '0.0325rem' }
 		}
 	}
 });
--- webui/src/material-ui.d.ts
+++ webui/src/material-ui.d.ts
@@ -1,4 +1,13 @@
 import type { PaletteOptions, Palette } from "@mui/material/styles/createPalette";
+import type { TypographyVariants, CSSProperties } from "@mui/material/styles/styles";
+import type { TypographyPropsVariantOverrides } from "@mui/material/Typography";
+
+type TypographyVariantsOptions = {
+  regular: React.CSSProperties;
+  medium: React.CSSProperties;
+  semiBold: React.CSSProperties;
+  bold: React.CSSProperties;
+}
 
 declare module "@mui/material/styles/createPalette" {
   export interface Palette {
@@ -186,6 +195,109 @@ declare module "@mui/material/styles/createPalette" {
     },
     disabled: {
       default: string;
+    }
+  }
+}
+
+declare module "@mui/material/styles" {
+  export interface TypographyVariants {
+    extraSmall: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    small: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    medium: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    large: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    extraLarge: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    }
+  }
+
+  export interface TypographyVariantsOptions {
+    extraSmall?: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    small?: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    medium?: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    large?: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    },
+    extraLarge?: {
+      regular: React.CSSProperties;
+      medium: React.CSSProperties;
+      semiBold: React.CSSProperties;
+      bold: React.CSSProperties;
+    }
+  }
+}
+
+declare module "@mui/material/Typography" {
+  export interface TypographyPropsVariantOverrides {
+    extraSmall: {
+      regular: true;
+      medium: true;
+      semiBold: true;
+      bold: true;
+    },
+    small: {
+      regular: true;
+      medium: true;
+      semiBold: true;
+      bold: true;
+    },
+    medium: {
+      regular: true;
+      medium: true;
+      semiBold: true;
+      bold: true;
+    },
+    large: {
+      regular: true;
+      medium: true;
+      semiBold: true;
+      bold: true;
+    },
+    extraLarge: {
+      regular: true;
+      medium: true;
+      semiBold: true;
+      bold: true;
     }
   }
 }
