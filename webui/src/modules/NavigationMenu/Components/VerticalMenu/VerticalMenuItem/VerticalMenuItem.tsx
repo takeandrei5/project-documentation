7653966147bf1ee1e6cd1d011126dbc2dc3d4d91
Andrei ~Tache <takeandrei5@gmail.com>
2023-09-11 23:40:32 +0300

    style: changes to vertical menu

--- webui/src/modules/NavigationMenu/Components/VerticalMenu/VerticalMenuItem/VerticalMenuItem.tsx
+++ webui/src/modules/NavigationMenu/Components/VerticalMenu/VerticalMenuItem/VerticalMenuItem.tsx
@@ -3,10 +3,10 @@ import type { VerticalMenuItemProps } from './types';
 
 const VerticalMenuItem: React.FC<VerticalMenuItemProps> = ({ color = '', shortcut = '', iconName = '', onClickHandler, text }) => {
 	return (
-		<MenuItem onClick={onClickHandler}>
+		<MenuItem onClick={onClickHandler} sx={{display: 'grid', gridTemplateColumns: '1.75rem 1fr 2.5rem'}}>
 			{!!iconName && (
-				<ListItemIcon>
-					<Icon sx={{ color: (theme: Theme) => !color ? theme.palette.textColor[60] : color }}>{iconName}</Icon>
+				<ListItemIcon sx={{marginRight: '0.25rem !important'}}>
+					<Icon sx={{ color: (theme: Theme) => !color ? theme.palette.textColor[60] : color, fontSize: '1.25rem !important' }}>{iconName}</Icon>
 				</ListItemIcon>
 			)}
 			<ListItemText>
